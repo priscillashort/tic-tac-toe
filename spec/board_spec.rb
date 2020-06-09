d8987113c8b1d604219486d6d5110fb21f723c8f
priscillashort <pshort2@cocc.edu>
2020-06-09 09:40:42 -0700

    Add token_at test for board

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,12 +6,48 @@ describe 'A Tic Tac Toe board' do
   #   fail 'See spec/board_spec.rb'
   # end
 
+  let(:full_board) {
+    Board.new({
+      top: {left: :fake, middle: :fake, right: :fake},
+      middle: {left: :fake, middle: :fake, right: :fake},
+      bottom: {left: :fake, middle: :fake, right: :fake}
+    })
+  }
+
+  let(:midgame_board) {
+    Board.new({
+      top: {left: :o, middle: nil, right: nil},
+      middle: {left: nil, middle: :x, right: nil},
+      bottom: {left: nil, middle: nil, right: nil}
+    })
+  }
+
+  let(:x_wins_board) {
+    Board.new({
+      top: {left: :x, middle: :x, right: :x},
+      middle: {left: :o, middle: :o, right: nil},
+      bottom: {left: :o, middle: nil, right: nil}
+    })
+  }
+
+  let(:o_wins_board) {
+    Board.new({
+      top: {left: :o, middle: :o, right: :o},
+      middle: {left: :x, middle: :x, right: nil},
+      bottom: {left: :x, middle: nil, right: nil}
+    })
+  }
+
   it 'exists' do
     Board.new
   end
 
   it 'has locations' do
     expect(Board.new).to respond_to(:locations)
+  end
+
+  it 'reports the token at a given location' do
+    expect(midgame_board.token_at(:top, :left)).to eq(:o)
   end
 
 end
